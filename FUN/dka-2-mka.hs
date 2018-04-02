f32bf2985fce88691b1595447a72d05101db8cc9
Jakub <jochlik@email.cz>
2018-04-02 22:25:21 +0200

    progress

--- FUN/dka-2-mka.hs
+++ FUN/dka-2-mka.hs
@@ -2,15 +2,16 @@ import System.Environment
 import System.IO
 import Data.List
 import Data.Char
+import Data.List.Split
 import qualified Data.Set as Set
-                                      
+
 
 -- naming stuff
 type State = String
 type Symbol = String
 type Alphabet = Set.Set Symbol
 
--- automat rules
+-- automat transition rules
 data Rule = Rule {
     currentState :: State,
     inputSymbol :: Symbol,
@@ -21,14 +22,22 @@ data Rule = Rule {
 data Automat = Automat {
     states :: Set.Set State,
     startingState :: State,
-    endStates :: Set.Set State,
-    alphabet :: Alphabet
+    endStates :: Set.Set State
+    --alphabet :: Alphabet,
+    --rules :: [Rule]
 }
-                                      
+
+-- for debugging
+instance Show Automat where
+    show (Automat states startingState endStates) =  
+        "states: " ++ show states ++ "\n " ++
+        "startingState: " ++ show startingState ++ "\n " ++
+        "endingStates: " ++ show endStates ++ "\n "
+
 parseArguments arguments = if length arguments < 1 || length arguments > 2 
     then error "Wrong parameters"
     else getArguments arguments
-            
+
 getArguments :: [String] -> Bool
 getArguments [] = error "Missing argument"
 getArguments [x]
@@ -41,16 +50,30 @@ getArguments [x, _]
     | otherwise = error "Unknown argument"
 getArguments _ = error "Wrong arguments"
 
+-- decide if input is gonna be file or stdin
 getInput arguments = if length arguments == 1
     then return stdin
     else openFile (last arguments) ReadMode
-    
-printDKA automat = do 
-    print "DKA"
-    let line = lines automat
-    print line
 
-printMKA automat = do 
+-- go through input (file) line by line and analyze incoming DKA
+parseAutomat :: [[Char]] -> Automat
+parseAutomat (states:start:end:rules) = Automat{
+    states = parseInputStates states,
+    startingState = parseStartingState start,
+    endStates = parseFinalStates end
+    }
+parseAutomat others = error "invalid automat"
+
+parseInputStates inputStates = Set.fromList $ splitOn "," inputStates
+
+parseStartingState startingState = startingState
+
+parseFinalStates finalStates = Set.fromList $ splitOn "," finalStates
+    
+printDKA inputAutomat = do 
+    print "DKA"
+
+printMKA inputAutomat = do 
     print "MKA"
  
 main :: IO() 
@@ -61,10 +84,12 @@ main = do
     input <- getInput arguments         -- read from file or stdin?
     content <- hGetContents input       -- read
     putStr content
+    let dka = parseAutomat $ lines content
+    print (show dka)
     
     if doReduce
-    then printMKA content          -- output MKA
-    else printDKA content     -- output analyzed DKA
+    then printMKA dka          -- output MKA
+    else printDKA dka     -- output analyzed DKA
     
     hClose input
     
