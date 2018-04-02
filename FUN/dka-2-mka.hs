a13274bfced241df2d199380d238ed99208960e4
Jakub <jochlik@email.cz>
2018-04-02 15:53:56 +0200

    data structures + gitignore

--- FUN/dka-2-mka.hs
+++ FUN/dka-2-mka.hs
@@ -2,8 +2,29 @@ import System.Environment
 import System.IO
 import Data.List
 import Data.Char
+import qualified Data.Set as Set
                                       
-            
+
+-- naming stuff
+type State = String
+type Symbol = String
+type Alphabet = Set.Set Symbol
+
+-- automat rules
+data Rule = Rule {
+    currentState :: State,
+    inputSymbol :: Symbol,
+    newState :: State
+}
+
+-- representation of KA
+data Automat = Automat {
+    states :: Set.Set State,
+    startingState :: State,
+    endStates :: Set.Set State,
+    alphabet :: Alphabet
+}
+                                      
 parseArguments arguments = if length arguments < 1 || length arguments > 2 
     then error "Wrong parameters"
     else getArguments arguments
@@ -23,15 +44,27 @@ getArguments _ = error "Wrong arguments"
 getInput arguments = if length arguments == 1
     then return stdin
     else openFile (last arguments) ReadMode
+    
+printDKA automat = do 
+    print "DKA"
+    let line = lines automat
+    print line
+
+printMKA automat = do 
+    print "MKA"
  
 main :: IO() 
 main = do
     arguments <- getArgs
     print arguments
-    let reduce = getArguments arguments     --select if i have to reduce the automat or just print it
+    let doReduce = getArguments arguments     --select if i have to reduce the automat or just print it
     input <- getInput arguments         -- read from file or stdin?
     content <- hGetContents input       -- read
     putStr content
+    
+    if doReduce
+    then printMKA content          -- output MKA
+    else printDKA content     -- output analyzed DKA
     
     hClose input
     
